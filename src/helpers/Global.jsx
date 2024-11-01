9c75d753c1078f8edb282bd0f4cf39071123c80f
Catalina Bejarano <catalina.bejarano@gmail.com>
2024-10-31 19:31:56 -0500

    Configure variable Global en Helpers

---
+++ src/helpers/Global.jsx
@@ -0,0 +1,15 @@
+//Ruta del Backend  desplegado Render 
+
+export const Global = {
+  url: "https://backend-social-net-2m41.onrender.com/api"
+
+  //Backend de la profe 
+  //url: "https://b3-backend-social-net.onrender.com/api/"
+
+}
+
+//Ruta del Backend  desplegado localmente
+//export const Global = {
+//    url: "https://localhost:3900/api"  
+//  }
+
